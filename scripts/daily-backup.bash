f9be014ab28440aefcb9b3002b5df95410e6880a
pjames <pjames@gmail.com>
2023-08-27 18:42:40 +0700

    Added Keycloak backup

--- scripts/daily-backup.bash
+++ scripts/daily-backup.bash
@@ -4,3 +4,4 @@ export $(xargs <../.env)
 
 ./backup.bash "promrub-${ENV}" "PG_SCB_USER" "PG_SCB_PASSWORD" "postgresql-scb-0" "scb"
 ./backup.bash "promjodd-${ENV}" "PG_CARPARK_API_USER" "PG_CARPARK_API_PASSWORD" "postgresql-carpark-api-0" "carpark-api"
+./backup.bash "promid-${ENV}" "PG_KEYCLOAK_USER" "PG_KEYCLOAK_PASSWORD" "keycloak-postgresql-0" "keycloak"
