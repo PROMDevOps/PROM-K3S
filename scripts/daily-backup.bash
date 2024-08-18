d51655b270116b54d742e6a4c5de9a85ad293497
pjames <pjames@gmail.com>
2024-08-19 04:12:03 +0700

    Added promrub backup command

--- scripts/daily-backup.bash
+++ scripts/daily-backup.bash
@@ -3,6 +3,7 @@
 export $(xargs <../.env)
 
 ./backup.bash "promrub-${ENV}" "PG_SCB_USER" "PG_SCB_PASSWORD" "postgresql-scb-0" "scb"
+./backup.bash "promrub-${ENV}" "PG_SCB_USER" "PG_SCB_PASSWORD" "postgresql-scb-0" "promrub"
 ./backup.bash "promjodd-${ENV}" "PG_CARPARK_API_USER" "PG_CARPARK_API_PASSWORD" "postgresql-carpark-api-0" "carpark-api"
 ./backup.bash "promid-${ENV}" "PG_KEYCLOAK_USER" "PG_KEYCLOAK_PASSWORD" "keycloak-postgresql-0" "keycloak"
 ./backup.bash "promerp-${ENV}" "PG_CARPARK_API_USER" "PG_CARPARK_API_PASSWORD" "postgresql-erp-0" "erp"
